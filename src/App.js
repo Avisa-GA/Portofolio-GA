e5c48f234ef670f0369e5ce4b3fcb39022174f99
Avisa-SEI <aap571420@gmail.com>
2021-06-21 18:03:32 -0700

    wip

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import 'materialize-css/dist/css/materialize.min.css';
 import NavBar from './Components/NavBar';
 import Subjects from './Components/Subjects';
@@ -12,36 +11,42 @@ return (
     <NavBar />
     <div style={{marginRight: '30%'}} className="container">
         <div class="row">
-             {/* ********* SectionOne ***************** */}
-             <div class="col s4">
-                 
-                <Subjects title="Dashboard" />
-                
-                <Subjects title="Widget"/>
-
-                <Subjects title="Reviews"/>
-
-                <Subjects title="Customers"/>
-
-                <Subjects title="Online Analysis"/>
-
-                <Subjects title="Settings"/>
-                
-            </div>
             {/* ********* SectionOne ***************** */}
             <div class="col s4">
-                <Subjects title="Reviews" number='1,281' />
-                <div class="divider"></div>
-                <Subjects title="Average Rating" number='4.6' />
-                <div class="divider"></div>
-                <SentimentAnal />
-                <div class="divider"></div>
+
+                <Subjects title="Dashboard" />
+
+                <Subjects title="Widget" />
+
+                <Subjects title="Reviews" />
+
+                <Subjects title="Customers" />
+
+                <Subjects title="Online Analysis" />
+
+                <Subjects title="Settings" />
+
+            </div>
+            {/* ********* SectionOne ***************** */}
+            <div style={{width: '500px'}} class="col s4">
+                <div style={{marginRight: '5%'}} class="col s3">
+                    <Subjects title="Reviews" number='1,281' />
+                </div>
+                {/* <div class="divider"></div> */}
+                <div style={{marginRight: '5%'}} class="col s3">
+                    <Subjects title="Average Rating" number='4.6' />
+                </div>
+                {/* <div class="divider"></div> */}
+                <div class="col s3">
+                    <SentimentAnal />
+                </div>
+                {/* <div class="divider"></div> */}
             </div>
 
 
             {/* *********** Section Two ******************* */}
             <div class="col s6">
-            <WebsiteVisitors />
+                <WebsiteVisitors />
             </div>
         </div>
     </div>
